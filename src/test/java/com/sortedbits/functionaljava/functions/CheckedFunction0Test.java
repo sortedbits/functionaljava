18ea6dc180ce0a1911897c10dca12295da1dc93b
sortedbits <1RandomBYte>
2016-10-21 17:47:59 +0200

    init

--- src/test/java/com/sortedbits/functionaljava/functions/CheckedFunction0Test.java
+++ src/test/java/com/sortedbits/functionaljava/functions/CheckedFunction0Test.java
@@ -1,10 +1,20 @@
 package com.sortedbits.functionaljava.functions;
 
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CheckedFunction0Test {
 
     @Test
-    public void test() {
+    public void testCheckedFunction0() {
+        CheckedFunction0<Integer> f = () -> 1;
+        assertThat(f.apply(), equalTo(1));
+    }
+
+    @Test(expected=FunctionException.class)
+    public void testCheckedFunction0Error() {
+        CheckedFunction0<Integer> f = () -> { throw new Exception("function error"); };
+        f.apply();
     }
 }
--- src/test/java/com/sortedbits/functionaljava/functions/Function0Test.java
+++ src/test/java/com/sortedbits/functionaljava/functions/Function0Test.java
@@ -1,29 +1,39 @@
 package com.sortedbits.functionaljava.functions;
 
-import com.sortedbits.functionaljava.functions.Function0;
-
-import com.sortedbits.functionaljava.functions.Function1;
+import com.sortedbits.functionaljava.suppliers.CheckedSupplier1;
 import com.sortedbits.functionaljava.suppliers.Supplier1;
+import com.sortedbits.functionaljava.suppliers.SupplierException;
 import org.junit.Test;
 
-import java.util.function.Function;
-
-import static com.sortedbits.functionaljava.functions.Function.function0;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Function0Test {
 
     @Test
-    public void testFunction() {
+    public void testFunction0() {
+        Function0<Integer> f = () -> 1;
+        assertThat(f.apply(), equalTo(1));
+    }
+
+    @Test
+    public void testSupplier1ToFunction0() {
         Supplier1<Integer> s = () ->  1;
         Function0<Integer> f = s.function0();
         assertThat(f.apply(), equalTo(1));
     }
 
     @Test
-    public void testFunction0() {
-        Function0<Integer> f = function0(() ->  1);
+    public void testCheckedSupplier1ToFunction0() {
+        CheckedSupplier1<Integer> s = () ->  1;
+        Function0<Integer> f = s.function0();
         assertThat(f.apply(), equalTo(1));
+    }
+
+    @Test(expected=SupplierException.class)
+    public void testCheckedSupplier1ToFunction0Error() {
+        CheckedSupplier1<?> s = () -> { throw new Exception("supplier exception"); };
+        Function0<Integer> f = s.function0();
+        f.apply();
     }
 }
